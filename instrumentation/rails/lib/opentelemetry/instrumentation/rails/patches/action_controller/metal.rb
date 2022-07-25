2cbca08b3fcc0d3bb5fd17561cddc7851b923d7b
rickzhan <81382009+rickzhan@users.noreply.github.com>
2022-07-25 11:17:21 -0400

    Update metal.rb
    update metal.rb file to add one more arg with relate to the error "Given 3, expected 2"

--- instrumentation/rails/lib/opentelemetry/instrumentation/rails/patches/action_controller/metal.rb
+++ instrumentation/rails/lib/opentelemetry/instrumentation/rails/patches/action_controller/metal.rb
@@ -11,10 +11,10 @@ module OpenTelemetry
         module ActionController
           # Module to prepend to ActionController::Metal for instrumentation
           module Metal
-            def dispatch(name, request)
+            def dispatch(name, request, response)
               rack_span = OpenTelemetry::Instrumentation::Rack.current_span
               rack_span.name = "#{self.class.name}##{name}" if rack_span.context.valid? && !request.env['action_dispatch.exception']
-              super(name, request)
+              super(name, request, response)
             end
           end
         end
